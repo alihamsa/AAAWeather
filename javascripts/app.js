273a5dd4a903ed3336af059193064ada0973a63a
Alejandro Gutierrez <alex.gtz17@gmail.com>
2015-11-07 10:19:54 -0600

    added require and index.html

---
+++ javascripts/app.js
@@ -0,0 +1,19 @@
+requirejs.config({
+    baseUrl: './javascripts',
+    paths: {
+        'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+        'lodash': '../lib/bower_components/lodash/lodash.min',
+        'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+        'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
+    },
+    shim: {
+        'bootstrap': ['jquery']
+    }
+});
+
+requirejs(
+    ["jquery", "hbs", "bootstrap"], function($, Handlebars, bootstrap) {
+
+
+
+    });
