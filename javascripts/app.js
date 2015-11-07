fd2d39b3d5bdabbb009abb4c78d5dbea86388fbb
Allen Phelps <allenatmc@gmail.com>
2015-11-07 13:51:08 -0600

    add search form

--- javascripts/app.js
+++ javascripts/app.js
@@ -12,13 +12,35 @@ requirejs.config({
 });
 
 requirejs(
-    ["jquery", "hbs", "bootstrap", "openWeather"], function($, Handlebars, bootstrap, openWeather) {
+    ["jquery", "hbs", "bootstrap", "openWeather", "searchBy"], function($, Handlebars, bootstrap, openWeather, searchBy) {
 
     var foo = function(x){
         y = $.parseJSON(x);
         console.log(y);
     };
 
-    openWeather.callAPI('37212', foo);
+
+    $(document).on('click','#search',function(e){
+        e.preventDefault();
+        zipInput = $('#zipSearch').val();
+        cityInput = $('#citySearch').val();
+
+        if (zipInput.length > 0){
+            searchBy.zip(foo);
+        } else if (cityInput.length > 0) {
+            searchBy.city(foo);
+        } else {
+            alert('Please input either Zip Code or City Name');
+        }
+
+    })
+
+    $(document).on('click','#reset',function(e){
+        e.preventDefault();
+        $('#zipSearch').val("");
+        $('#citySearch').val("");
+
+    })
+
 
     });
